4a7e90cadd70d38ec5ad9a83b80243aaa81f8d2f
jlibi <sudtidath@gmail.com>
2025-04-24 01:31:09 -0400

    Fix Build logs successfully :).

--- app/r/[alias]/page.tsx
+++ app/r/[alias]/page.tsx
@@ -2,7 +2,7 @@ import { getUrl } from "@/lib/url";
 import { redirect } from "next/navigation";
 
 export default async function RedirectPage({ params }: { params: Promise<{ alias: string }> }) {
-    // Await params—if it's not a Promise it will be wrapped in Promise.resolve
+
     const resolvedParams = await params;
     console.log("Alias received:", resolvedParams.alias);
 
